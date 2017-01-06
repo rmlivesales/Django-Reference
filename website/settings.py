60d66d3a06ce111fcb9e302eb5b3ef8d656df680
rmlivesales <rm_livestock_s@yahoo.com>
2017-01-06 16:29:36 +0200

    Planned upsate

--- website/settings.py
+++ website/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'cattlecrm.urls'
+ROOT_URLCONF = 'website.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'cattlecrm.wsgi.application'
+WSGI_APPLICATION = 'website.wsgi.application'
 
 
 # Database
